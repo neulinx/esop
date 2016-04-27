30a7b4560de6c21b07dfac1863b0461407983557
garyhai <gary@XL59.com>
2016-04-27 13:43:21 +0800

    Generate by emacs mode.

--- apps/xlp/src/xlp_app.erl
+++ apps/xlp/src/xlp_app.erl
@@ -1,8 +1,11 @@
 %%%-------------------------------------------------------------------
-%% @doc xlp public API
-%% @end
+%%% @author Guiqing Hai <gary@XL59.com>
+%%% @copyright (C) 2016, Guiqing Hai
+%%% @doc
+%%%
+%%% @end
+%%% Created : 27 Apr 2016 by Guiqing Hai <gary@XL59.com>
 %%%-------------------------------------------------------------------
-
 -module(xlp_app).
 
 -behaviour(application).
@@ -10,17 +13,47 @@
 %% Application callbacks
 -export([start/2, stop/1]).
 
-%%====================================================================
-%% API
-%%====================================================================
+%%%===================================================================
+%%% Application callbacks
+%%%===================================================================
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called whenever an application is started using
+%% application:start/[1,2], and should start the processes of the
+%% application. If the application is structured according to the OTP
+%% design principles as a supervision tree, this means starting the
+%% top supervisor of the tree.
+%%
+%% @spec start(StartType, StartArgs) -> {ok, Pid} |
+%%                                      {ok, Pid, State} |
+%%                                      {error, Reason}
+%%      StartType = normal | {takeover, Node} | {failover, Node}
+%%      StartArgs = term()
+%% @end
+%%--------------------------------------------------------------------
 start(_StartType, _StartArgs) ->
-    xlp_sup:start_link().
+    case xlp_sup:start_link() of
+        {ok, Pid} ->
+            {ok, Pid};
+        Error ->
+            Error
+    end.
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called whenever an application has stopped. It
+%% is intended to be the opposite of Module:start/2 and should do
+%% any necessary cleaning up. The return value is ignored.
+%%
+%% @spec stop(State) -> void()
+%% @end
 %%--------------------------------------------------------------------
 stop(_State) ->
     ok.
 
-%%====================================================================
-%% Internal functions
-%%====================================================================
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
--- apps/xlp/src/xlp_sup.erl
+++ apps/xlp/src/xlp_sup.erl
@@ -1,8 +1,11 @@
 %%%-------------------------------------------------------------------
-%% @doc xlp top level supervisor.
-%% @end
+%%% @author Guiqing Hai <gary@XL59.com>
+%%% @copyright (C) 2016, Guiqing Hai
+%%% @doc
+%%%
+%%% @end
+%%% Created : 27 Apr 2016 by Guiqing Hai <gary@XL59.com>
 %%%-------------------------------------------------------------------
-
 -module(xlp_sup).
 
 -behaviour(supervisor).
@@ -15,21 +18,52 @@
 
 -define(SERVER, ?MODULE).
 
-%%====================================================================
-%% API functions
-%%====================================================================
+%%%===================================================================
+%%% API functions
+%%%===================================================================
 
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the supervisor
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
-%%====================================================================
-%% Supervisor callbacks
-%%====================================================================
+%%%===================================================================
+%%% Supervisor callbacks
+%%%===================================================================
 
-%% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever a supervisor is started using supervisor:start_link/[2,3],
+%% this function is called by the new process to find out about
+%% restart strategy, maximum restart intensity, and child
+%% specifications.
+%%
+%% @spec init(Args) -> {ok, {SupFlags, [ChildSpec]}} |
+%%                     ignore |
+%%                     {error, Reason}
+%% @end
+%%--------------------------------------------------------------------
 init([]) ->
-    {ok, { {one_for_all, 0, 1}, []} }.
 
-%%====================================================================
-%% Internal functions
-%%====================================================================
+    SupFlags = #{strategy => one_for_one,
+                 intensity => 1,
+                 period => 5},
+
+    AChild = #{id => xl_state,
+               start => {xl_state, start_link, []},
+               restart => permanent,
+               shutdown => 5000,
+               type => worker,
+               modules => [xl_state]},
+
+    {ok, {SupFlags, [AChild]}}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
