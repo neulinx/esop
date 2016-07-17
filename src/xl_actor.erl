3f8f7b23287e7b0f07b704e57a909dfca9350116
garyhai <gary@XL59.com>
2016-07-17 18:13:30 +0800

    Maintainable code is more important than the code efficiency.

--- src/xl_actor.erl
+++ src/xl_actor.erl
@@ -60,7 +60,11 @@
 -spec entry(actor()) -> {'ok', actor()}.
 entry(Actor) ->
     process_flag(trap_exit, true),
-    preload(Actor).
+    Links = maps:get(links, Actor, #{}),
+    Monitors = maps:get(monitors, Actor, #{}),
+    Timeout = maps:get(timeout, Actor, ?DFL_TIMEOUT),
+    A = Actor#{links => Links, monitors => Monitors, timeout => Timeout},
+    preload(A).
 
 %%--------------------------------------------------------------------
 %% Uninitialize, stop all local actors.
@@ -90,7 +94,7 @@ react({'DOWN', Monitor, process, _, _Output}, Actor) ->
 %% Notice: it is a backdoor if PID in path, so remove it.
 react({xlx, From, [Next | Path], Message}, Actor) ->
     %% relay to next hop
-    case ensure_link(Next, Actor, true) of
+    case ensure_load(Next, Actor) of
         {ok, Pid, A} ->
             Pid ! {xlx, From, Path, Message},
             {ok, A};
@@ -114,17 +118,16 @@ react({xlx, From, Command}, Actor) ->
 react(_Message, Actor) ->
     {ok, {error, unhandled}, Actor}.
 
-invoke({get_link, Id}, Actor) ->
-    ensure_link(Id, Actor, true);
+invoke({load, Link}, Actor) ->
+    ensure_load(Link, Actor);
 invoke({link, Link}, Actor) ->
-    ensure_link(Link, Actor, false);
-invoke({link, Link, Load}, Actor) ->
-    ensure_link(Link, Actor, Load);
-invoke({register, Link, Load}, Actor) ->
-    ensure_link(Link, Actor, Load);
-invoke({deregister, Name} = Command, #{parent := R} = Actor) ->
-    Timeout = maps:get(timeout, Actor, ?DFL_TIMEOUT),
-    Result = case xl_state:call(R, Command, Timeout) of
+    ensure_link(Link, Actor);
+invoke({link, Link, true}, Actor) ->
+    ensure_load(Link, Actor);
+invoke({link, Link, _}, Actor) ->
+    ensure_link(Link, Actor);
+invoke({unlink, Name} = Command, #{parent := R, timeout := T} = Actor) ->
+    Result = case xl_state:call(R, Command, T) of
                  {ok, Res} ->
                      Res;
                  Error ->
@@ -136,8 +139,6 @@ invoke({deregister, Name} = Command, #{parent := R} = Actor) ->
         _Error ->
             {ok, Result, Actor}
     end;
-invoke({deregister, Name}, Actor) ->
-    unlink(Name, Actor);
 invoke({unlink, Name}, Actor) ->
     unlink(Name, Actor);
 %% default behavior is data access
@@ -208,7 +209,7 @@ preload(Actor) ->
     {ok, Actor}.
 
 preload([Dep | Rest], Actor) ->
-    {ok, _, A} = ensure_link(Dep, Actor, true),  % link & load
+    {ok, _, A} = ensure_load(Dep, Actor),  % link & load
     preload(Rest, A);
 preload([], Actor) ->
     {ok, Actor};
@@ -216,51 +217,76 @@ preload([], Actor) ->
 preload(Preload, Actor) when is_function(Preload) ->
     Preload(Actor).
 
-ensure_link({Name, State}, Actor, Load) ->
-    ensure_link(Name, State, Actor, Load);
-ensure_link(#{gid := Gid} = State, Actor, Load) ->
-    ensure_link(Gid, State, Actor, Load);
-ensure_link(#{name := Name} = State, Actor, Load) ->
-    ensure_link(Name, State, Actor, Load);
-ensure_link(Id, Actor, Load) ->
-    ensure_link(Id, Id, Actor, Load).
+flat_link({Name, State}) ->
+    {Name, State};
+flat_link(#{gid := Gid} = State) ->
+    {Gid, State};
+flat_link(#{name := Name} = State) ->
+    {Name, State};
+flat_link(Id) ->
+    {Id, undefined}.
 
-ensure_link(_Name, Link, #{boundary := B}, _Load)
-  when B =/= opened andalso is_map(Link) ->
-    %% boudary is closed, link cannot be changed.
-    {error, forbidden};
+ensure_load(Link, Actor) ->
+    {Name, State} = flat_link(Link),
+    ensure_link(Name, State, Actor, true).
+
+ensure_link(Link, Actor) ->
+    {Name, State} = flat_link(Link),
+    ensure_link(Name, State, Actor, false).
+
 ensure_link(Name, Link, #{links := L} = Actor, Load) ->
-    case maps:find(Name, L) of
-        error ->
-            link_(Name, Link, Actor, Load);  % not registered
-        {ok, {Link, Pid}} ->  % local active link
-            {ok, Pid, Actor};
-        {ok, {Link, Pid, _}} ->  % global active link
-            {ok, Pid, Actor};
-        {ok, Link} when Load ->  % registered but inactive link
-            link_(Name, Link, Actor, Load);
-        {ok, Link} ->  % already registered, but same data. It is ok.
-            {ok, Name, Actor};
-        {ok, {_Gid, Pid, _}} ->  % local name link global actor;
-            {ok, Pid, Actor};
-        {ok, {_State, Pid}} ->  % local name link local actor;
-            {ok, Pid, Actor};
-        {ok, State} when Load ->  % local inactive link
+    B = maps:get(boundary, Actor, closed),  % default realm is closed.
+    V =  maps:find(Name, L),
+    case check_link(Link, V, B) of
+        {ok, Id} ->
+            {ok, Id, Actor};
+        {link, State} ->
             link_(Name, State, Actor, Load);
-        {ok, _Existed} ->  % Link is different, is conflict.
-            {error, existed}
-    end;
-ensure_link(Name, Link, Actor, Load) ->
-    link_(Name, Link, Actor#{links => #{}}, Load).  % no register entry
+        link ->
+            link_(Name, Name, Actor, Load);
+        Error ->
+            Error
+    end.
+
+%% check boundary
+check_link(_, error, Boundary) when Boundary =/= opened ->
+    {error, forbidden};
+check_link(Link, error, _) ->
+    check_link(Link, undefined);
+check_link(Link, {ok, V}, _) ->
+    check_link(Link, V).
+
+%% compare links data.
+check_link(undefined, undefined) ->  %  maybe global link.
+    link;
+check_link(undefined, {_, Pid}) ->  % local active link
+    {ok, Pid};
+check_link(undefined, {_, Pid, _}) ->  % global active link
+    {ok, Pid};
+check_link(undefined, State) ->  % inactive link
+    {link, State};
+check_link(Link, undefined) ->  % not registered
+    {link, Link};
+check_link(Link, Link) ->  % inactive link
+    {link, Link};
+check_link(Link, {Link, Pid}) ->  % active local link
+    {ok, Pid};
+check_link(Link, {Link, Pid, _}) ->  % active global link
+    {ok, Pid};
+check_link(Gid, #{gid := Gid} = State) ->  % not registered global link
+    {link, State};
+check_link(Gid, {#{gid := Gid}, Pid}) -> % no parent, it is realm keeper.
+    {ok, Pid};
+check_link(_, _) ->
+    {error, existed}.
 
 %% If gid is presented, it is global actor, fetch from realm keeper.
 %% Or current actor acts as realm keeper if no present realm.
 link_(Name, #{gid := Gid} = State, #{parent := R} = Actor, Load) ->
     g_link(Name, Gid, R, {link, {Gid, State}, Load}, Actor);
-link_(Name, #{} = State, #{links := L} = Actor, true) ->
+link_(Name, #{} = State, #{links := L, monitors := M} = Actor, true) ->
     {ok, Member} = spawn_actor(State, Actor),
     L1 = L#{Name => {State, Member}},
-    M = maps:get(monitors, Actor, #{}),
     M1 = M#{Member => Name},
     A = Actor#{links := L1, monitors => M1},
     {ok, Member, A};
@@ -272,15 +298,14 @@ link_(Name, Gid, #{parent := R} = Actor, Load) ->
 link_(_N, _S, _A, _L) ->
     {error, invalid}.
 
-g_link(Name, Gid, Realm, Command, #{links := L} = Actor) ->
-    Timeout = maps:get(timeout, Actor, ?DFL_TIMEOUT),
-    case xl_state:call(Realm, Command, Timeout) of
+g_link(Name, Gid, Realm, Command,
+       #{links := L, monitors := M, timeout := T} = Actor) ->
+    case xl_state:call(Realm, Command, T) of
         {ok, Gid} ->  % not active
             A = Actor#{links := L#{Name => Gid}},
             {ok, Gid, A};
         {ok, Pid} ->
             Mid = monitor(process, Pid),
-            M = maps:get(monitors, Actor, #{}),
             A = Actor#{links := L#{Name => {Gid, Pid, Mid}},
                        monitors => M#{Mid => Name}},
             {ok, Pid, A};
@@ -324,8 +349,7 @@ deactive(Pid) when is_pid(Pid) ->
 deactive(Monitor) ->
     demonitor(Monitor).
 
-unlink(Name, #{boundary := opened, links := L} = Actor) ->
-    M = maps:get(monitors, Actor, #{}),
+unlink(Name, #{boundary := opened, links := L, monitors := M} = Actor) ->
     case maps:find(Name, L) of
         {ok, {_S, Pid}} ->  % local link
             deactive(Pid),
@@ -360,14 +384,13 @@ g1_test() ->
     B = new_actor(#{gid => 2, name => b, links => Bl}),
     Al = #{b => B, c => C},
     A = new_actor(#{gid => 1, name => a, links => Al}),
-    {ok, Aa} = xl_state:call(Ra, {register, A, true}),
+    {ok, Aa} = xl_state:call(Ra, {load, A}),
     ?assertMatch({ok, a}, xl_state:call(Aa, name)),
     ?assertMatch({ok, c}, call(Aa, [c], name)),
     ?assertMatch({ok, c}, call(Aa, [b, c], name)),
     ?assertMatch({ok, x}, call(Aa, [b, x], name)),
-    ?debugVal(xl_state:call(Ra, links)),
-    ?debugVal(xl_state:call(Aa, links)),
-
+%    ?debugVal(xl_state:call(Ra, links)),
+%    ?debugVal(xl_state:call(Aa, links)),
     xl_state:stop(Aa),
     xl_state:stop(Ra).
     
